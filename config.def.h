2f03d518da9e0da42336deda73dc012a9e9c348e
Adithya Nair <adtya.nair@gmail.com>
2020-04-06 09:45:42 +0530

    dracula theme and default dmenu

--- config.def.h
+++ config.def.h
@@ -6,16 +6,21 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code:size=12" };
-static const char dmenufont[]       = "Fira Code:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_background[]	= "#282a36";
+static const char col_foreground[]	= "#f8f8f2";
+static const char col_selection[]	= "#44475a";
+static const char col_unfocused[]	= "#62724a";
+static const char col_cyan[]		= "#8be9fd";
+static const char col_green[]		= "#50fa7b";
+static const char col_orange[]		= "#ffb86c";
+static const char col_pink[]		= "#ff79c6";
+static const char col_purple[]		= "#bd93f9";
+static const char col_red[]			= "#ff5555";
+static const char col_yellow[]		= "#f1fa8c";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg				 bg				 border   */
+	[SchemeNorm] = { col_foreground, col_background, col_unfocused },
+	[SchemeSel]  = { col_selection,  col_cyan,       col_green },
 };
 
 /* tagging */
@@ -55,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include<X11/XF86keysym.h>
